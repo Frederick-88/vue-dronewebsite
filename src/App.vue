82f870a35b78dac24cf28a8fe95504eb763737f4
Frederick-88 <frederickfd88@gmail.com>
2020-07-30 21:27:13 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,18 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <v-btn style="background-color: orange;" large>Large Button</v-btn>
-    <v-btn class="ma-2" outlined fab color="teal">
-      <v-icon>mdi-format-list-bulleted-square</v-icon>
-    </v-btn>
-    <div class="text-center">
-      <v-rating
-        background-color="green lighten-3"
-        color="green"
-        v-model="rating"
-      ></v-rating>
-    </div>
     <router-view />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Hi this is home with vue bootstrap</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
