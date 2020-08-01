ee2c3908408c3a84d9d7df8e44122b91f946cbd3
Frederick-88 <frederickfd88@gmail.com>
2020-08-01 12:49:24 +0700

    Done Footer

--- src/components/Home/Footer.vue
+++ src/components/Home/Footer.vue
@@ -1,78 +1,69 @@
 <template>
-  <div className="mx-5 my-5">
-    <div className="row">
-      <div className="col-md-2">
-        <img :src="navImage" alt="..." className="w-100" />
-      </div>
-      <div className="col-md-3">
-        <p className="footer-title">S2 BOUTIQUE</p>
-        <small>
-          Look inside trendy,elegant & fantastic fashion in Batam. Tell the
-          world who you are with style without having to speak.
-        </small>
-        <div className="mt-4">
-          <small className="pt-4">© 2020 S2-Boutique Batam</small>
-        </div>
-      </div>
+  <div class="mx-1 footer-bg" :style="{ backgroundImage: `url(${footerImage})` }">
+    <div class="text-center">
+      <img :src="navImage" alt="..." style="width:20rem" />
+    </div>
+    <p
+      class="text-center text-secondary"
+      style="margin:0 12rem"
+    >As the market leader in easy-to-fly drones and aerial photography systems, Azur Drones' quadcopters like the Phantom are the standard in consumer drone technology.</p>
+    <div class="d-flex justify-content-center mt-4">
+      <button class="btn btn-outline-success mr-3">
+        <i class="fab fa-whatsapp fa-lg" />
+      </button>
 
-      <div className="col-md-3">
-        <p className="footer-title">REACH OUT</p>
-        <button className="btn btn-outline-success mr-3">
-          <i className="fab fa-whatsapp fa-lg" />
-        </button>
+      <button class="btn btn-outline-instagram mr-3">
+        <i class="fab fa-instagram fa-lg" />
+      </button>
+      <button class="btn btn-outline-primary mr-3">
+        <i class="fab fa-facebook-square fa-lg" />
+      </button>
 
-        <button className="btn btn-outline-instagram mr-3">
-          <i className="fab fa-instagram fa-lg" />
-        </button>
-        <button className="btn btn-outline-primary mr-3">
-          <i className="fab fa-facebook-square fa-lg" />
-        </button>
-
-        <button className="btn btn-outline-danger">
-          <i className="far fa-envelope fa-lg" />
-        </button>
-      </div>
-      <div className="col-md-4">
-        <p className="footer-title">SUBSCRIBE</p>
-        <small>
-          Don't miss out the Latest Sale, Discounts, and New Arrivals !!
-        </small>
-        <div className="input-group mt-3 mb-2">
-          <input
-            type="email"
-            className="form-control"
-            name="emailUser"
-            placeholder="Your Gmail/Email"
-          />
-          <div className="input-group-append">
-            <button
-              className="btn btn-outline-success"
-              type="button"
-              id="button-addon2"
-            >
-              <i className="fas fa-chevron-circle-right"></i>
-            </button>
-          </div>
-        </div>
-        <small className="text-secondary">
-          We will not share your gmail/whatsapp to anyone else.
-        </small>
-      </div>
+      <button class="btn btn-outline-danger">
+        <i class="far fa-envelope fa-lg" />
+      </button>
+    </div>
+    <div class="mt-4 border-top">
+      <p class="text-center pt-3 text-secondary">
+        Copyright © 2020. All rights reserved by
+        <span class="icon-fx">ChenFrederick</span>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
 import navImage from "../../assets/logo.png";
+import footerImage from "../../assets/footer-bg.png";
 export default {
   name: "FeaturesPage",
   components: {},
   data() {
     return {
       navImage: navImage,
+      footerImage: footerImage
     };
-  },
+  }
 };
 </script>
 
-<style></style>
+<style>
+.btn-outline-instagram {
+  color: #fb3958 !important;
+  border-color: #fb3958 !important;
+}
+.btn-outline-instagram:hover {
+  color: #fff !important;
+  background-color: #fb3958 !important;
+  border-color: #fb3958 !important;
+}
+.btn-outline-instagram:focus {
+  box-shadow: 0 0 0 0.2rem rgba(220, 53, 69, 0.5) !important;
+}
+.footer-bg {
+  background-size: cover;
+  background-position: center;
+  height: 100%;
+  padding: 2rem 0;
+}
+</style>
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -14,16 +14,10 @@
       <div class="border-top mx-3">
         <div class="row" style="margin-top: 7rem;">
           <div class="col-6">
-            <img
-              src="../../assets/mydrone1.png"
-              alt="image"
-              class="drone-animation-fx"
-            />
+            <img src="../../assets/mydrone1.png" alt="image" class="drone-animation-fx" />
           </div>
           <div class="col-6 pl-4">
-            <h1 class="display-4 title-fx text-white">
-              Best quality Drone for your daily life
-            </h1>
+            <h1 class="display-4 title-fx text-white">Best quality Drone for your daily life</h1>
             <h5 class="text-white mt-4 pr-5">
               As the market leader in easy-to-fly drones and aerial photography
               systems, Azur Drones' quadcopters like the Phantom are the
@@ -36,16 +30,8 @@
           </div>
         </div>
       </div>
-      <svg
-        class="bottom-fx"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          fill="#ffffff"
-          fill-opacity="1"
-          d="M0,96L720,320L1440,96L1440,320L720,320L0,320Z"
-        />
+      <svg class="bottom-fx" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#ffffff" fill-opacity="1" d="M0,96L720,320L1440,96L1440,320L720,320L0,320Z" />
       </svg>
     </div>
 
@@ -101,24 +87,15 @@
     <FeaturesPage />
 
     <!-- Section 5 -->
-    <div
-      class="subscribe-bg"
-      :style="{ backgroundImage: `url(${backgroundImage})` }"
-    >
+    <div class="subscribe-bg" :style="{ backgroundImage: `url(${backgroundImage})` }">
       <div class="text-center">
-        <h2 class="font-weight-bold mb-3 text-white">
-          Subscribe To Get Updates
-        </h2>
+        <h2 class="font-weight-bold mb-3 text-white">Subscribe To Get Updates</h2>
         <p class="text-white mb-5">
           Prepared is me marianne pleasure likewise debating. Wonder an unable
           except better stairs do ye admire. His secure called esteem praise.
         </p>
         <div class="input-group">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Your email address..."
-          />
+          <input type="text" class="form-control py-4" placeholder="Your email address..." />
           <div class="input-group-append">
             <button class="btn btn-secondary" type="button" id="button-addon2">
               Subscribe
@@ -147,17 +124,17 @@ export default {
   components: {
     CarouselPage,
     FeaturesPage,
-    FooterSection,
+    FooterSection
   },
   data() {
     return {
       image: navImage,
-      backgroundImage: backgroundImage,
+      backgroundImage: backgroundImage
     };
   },
   mounted() {
     console.log("this is rendered in the first place");
-  },
+  }
 };
 </script>
 
