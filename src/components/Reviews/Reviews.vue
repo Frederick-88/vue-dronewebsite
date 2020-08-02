eb283ed614461fc4d62c7f3a5433b7b411ef3a5e
Frederick-88 <frederickfd88@gmail.com>
2020-08-02 21:50:12 +0700

    Mounted Axios Get

--- src/components/Reviews/Reviews.vue
+++ src/components/Reviews/Reviews.vue
@@ -3,72 +3,70 @@
     <Navbar />
     <div style="border-top:2px solid #2c3e50;">
       <div class="text-center" style="margin:2rem 16rem">
-        <h3 class="icon-fx font-weight-bold">Review Form</h3>
-        <h5 class="mt-4 font-weight-bold">1. Pick Your Character</h5>
-        <div class="row d-flex justify-content-center">
-          <div
-            v-for="(character, index) in characters"
-            :key="index"
-            class="col-3 azur-border mx-2"
-            :class="{
-              activeBorder: selectedCharacter === character.characterGender,
-            }"
-            @click="updateSelectedCharacter(character.characterGender)"
-          >
-            <img :src="character.characterImage" alt="char-image" class="w-100" />
+        <form @submit.prevent="submitReview">
+          <h3 class="icon-fx font-weight-bold">Review Form</h3>
+          <h5 class="mt-4 font-weight-bold">1. Pick Your Character</h5>
+          <div class="row d-flex justify-content-center">
+            <div
+              v-for="(character, index) in characters"
+              :key="index"
+              class="col-3 azur-border mx-2"
+              :class="{
+                activeBorder: selectedCharacter === character.characterGender,
+              }"
+              @click="updateSelectedCharacter(character.characterGender)"
+            >
+              <img
+                :src="character.characterImage"
+                alt="char-image"
+                class="w-100"
+              />
+            </div>
           </div>
-        </div>
-        <div>
-          <h5 class="mt-4 font-weight-bold">2. Fill This Form</h5>
-          <p class="mt-3 mb-1">Title of Review</p>
-          <input
-            name="title"
-            type="text"
-            class="w-75 py-2 px-3"
-            placeholder="Title of Review"
-            v-model="dataInput.title"
-          />
-          <p class="mt-3 mb-1">Description of Review</p>
-          <input
-            name="description"
-            type="text"
-            class="w-75 py-2 px-3"
-            placeholder="Share with us how you felt on AzurDrones' Products!"
-            v-model="dataInput.description"
-          />
-        </div>
+          <div>
+            <h5 class="mt-4 font-weight-bold">2. Fill This Form</h5>
+
+            <p class="mt-3 mb-1">Headline of Review</p>
+            <input
+              name="headline"
+              type="text"
+              class="w-75 py-2 px-3"
+              placeholder="Headline of Review"
+              v-model="dataInput.title"
+            />
+            <p class="mt-3 mb-1">Description of Review</p>
+            <input
+              name="description"
+              type="text"
+              class="w-75 py-2 px-3"
+              placeholder="Share with us how you felt on AzurDrones' Products!"
+              v-model="dataInput.description"
+            />
+          </div>
+          <button type="submit" class="btn button-fx text-white mt-4">
+            Submit Review
+          </button>
+        </form>
       </div>
 
+      <!-- Review Data -->
       <div class="text-center" style="margin:2rem 16rem">
         <h3 class="icon-fx font-weight-bold">The Review Data belongs here</h3>
         <div class="row mb-4">
-          <div class="col-4 mt-4">
-            <div class="special-card">
-              <div class="d-flex justify-content-center">
-                <img
-                  src="http://geniusdevs.com/themeforest/prolab/probucket/assets/images/testimonialimage/1.jpg"
-                  alt="char-img"
-                  class="rounded-circle"
-                  style
-                />
-              </div>
-              <div class="card">
-                <div class="card-body pt-5">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p>From: AzureBay77</p>
-                  <p
-                    class="card-text"
-                  >With supporting text below as a natural lead-in to additional content.</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <div
+            v-for="(review, index) in dataReview"
+            :key="index"
+            class="col-4 mt-4"
+          >
+            <!-- Try To Make Loading -->
+            <!-- <div v-if="loadingDataReview"><h1>Loading...</h1></div> -->
+            <!-- <div v-else>Not Loading...</div> -->
 
-          <div class="col-4 mt-4">
             <div class="special-card">
               <div class="d-flex justify-content-center">
+                <!-- In my backend, ImageBook = Image(At Here) -->
                 <img
-                  src="http://geniusdevs.com/themeforest/prolab/probucket/assets/images/testimonialimage/1.jpg"
+                  :src="review.imageBook"
                   alt="char-img"
                   class="rounded-circle"
                   style
@@ -76,17 +74,17 @@
               </div>
               <div class="card">
                 <div class="card-body pt-5">
-                  <h5 class="card-title">Special title treatment</h5>
-                  <p>From: AzureBay77</p>
-                  <p
-                    class="card-text"
-                  >With supporting text below as a natural lead-in to additional content.</p>
+                  <!-- In my backend, BookTitle = Title(At Here) -->
+                  <h5 class="card-title">{{ review.bookTitle }}</h5>
+                  <!-- In my backend, BookNumber = Description(At Here) -->
+                  <p class="card-text">
+                    {{ review.bookNumber }}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <button class="btn button-fx text-white">Button</button>
       </div>
     </div>
     <Footer />
@@ -96,12 +94,28 @@
 <script>
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Home/Footer";
+import axios from "axios";
+
+const url = "https://fd-library.herokuapp.com";
 
 export default {
   name: "Reviews",
   components: {
     Navbar,
-    Footer
+    Footer,
+  },
+  // Mounted = UseEffect/ComponentDidMount in ReactJS
+  mounted() {
+    axios
+      .get(`${url}/library/get`)
+      .then((res) => {
+        this.dataReview = res.data.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    // try to make loading page in it.
+    // .finally(() => (this.loadingDataReview = false));
   },
   data() {
     return {
@@ -110,25 +124,32 @@ export default {
         {
           characterGender: "Man",
           characterImage:
-            "http://geniusdevs.com/themeforest/prolab/probucket/assets/images/testimonialimage/1.jpg"
+            "http://geniusdevs.com/themeforest/prolab/probucket/assets/images/testimonialimage/1.jpg",
         },
         {
           characterGender: "Women",
           characterImage:
-            "http://geniusdevs.com/themeforest/prolab/probucket/assets/images/testimonialimage/2.jpg"
-        }
+            "http://geniusdevs.com/themeforest/prolab/probucket/assets/images/testimonialimage/2.jpg",
+        },
       ],
       dataInput: {
-        title: "",
-        description: ""
-      }
+        Headline: "",
+        description: "",
+      },
+      // From Axios
+      dataReview: null,
+      // loadingDataReview: true,
     };
   },
   methods: {
-    updateSelectedCharacter(data) {
-      this.selectedCharacter = data;
-    }
-  }
+    // update the character gender
+    updateSelectedCharacter(selected) {
+      this.selectedCharacter = selected;
+    },
+    submitReview() {
+      // let inputReview = {};
+    },
+  },
 };
 </script>
 
