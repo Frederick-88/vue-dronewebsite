9706a937d017389f5922a60931b9749ac4b92406
Frederick <frederickfd88@gmail.com>
2020-09-29 17:12:44 +0700

    routine push

--- src/components/Reviews/Reviews.vue
+++ src/components/Reviews/Reviews.vue
@@ -5,7 +5,7 @@
       <div class="text-center" style="margin:2rem 16rem">
         <form @submit.prevent="submitReview">
           <h3 class="icon-fx font-weight-bold">Review Form</h3>
-          <h5 class="mt-4 font-weight-bold">1. Pick Your Charaacter</h5>
+          <h5 class="mt-4 font-weight-bold">1. Pick Your Character</h5>
           <div class="row d-flex justify-content-center">
             <div
               v-for="(character, index) in characters"
